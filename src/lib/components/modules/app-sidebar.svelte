f4386dbd2d1a82f9c353e9e3d7ed7826f2092fbe
jonas_school <jonas.coding.all@gmail.com>
2025-03-27 18:00:46 +0100

    Added Sidebar

---
+++ src/lib/components/modules/app-sidebar.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+	import House from '@lucide/svelte/icons/house';
+	import MessageCircle from '@lucide/svelte/icons/message-circle';
+	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
+	import NavUser from './nav-user.svelte';
+
+	const data = {
+		user: {
+			name: 'Jonas',
+			email: 'jonas.coding.all@gmail.com',
+			avatar: 'https://avatar.iran.liara.run/public/15'
+		},
+		items: [
+			{
+				title: 'Home',
+				url: '/home',
+				icon: House
+			},
+			{
+				title: 'Chats',
+				url: '/chats',
+				icon: MessageCircle
+			}
+		]
+	};
+</script>
+
+<Sidebar.Root collapsible="icon">
+	<Sidebar.Header>
+		<NavUser user={data.user} />
+	</Sidebar.Header>
+	<Sidebar.Content>
+		<Sidebar.Group>
+			<Sidebar.GroupLabel>ScribNET</Sidebar.GroupLabel>
+			<Sidebar.GroupContent>
+				<Sidebar.Menu>
+					{#each data.items as item (item.title)}
+						<Sidebar.MenuItem>
+							<Sidebar.MenuButton>
+								{#snippet child({ props })}
+									<a href={item.url} {...props}>
+										<item.icon />
+										<span>{item.title}</span>
+									</a>
+								{/snippet}
+							</Sidebar.MenuButton>
+						</Sidebar.MenuItem>
+					{/each}
+				</Sidebar.Menu>
+			</Sidebar.GroupContent>
+		</Sidebar.Group>
+	</Sidebar.Content>
+</Sidebar.Root>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,12 +2,8 @@
 	import { Button } from '$lib/components/ui/button';
 	import '../app.css';
 	import { ModeWatcher } from 'mode-watcher';
-	import { toggleMode } from 'mode-watcher';
 	let { children } = $props();
 </script>
 
 <ModeWatcher />
-<Button onclick={toggleMode}>Toggle Mode</Button>
-<div class="flex h-screen items-center justify-center">
-	{@render children()}
-</div>
+{@render children()}
